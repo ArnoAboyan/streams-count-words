13e04f8486473435c73e7fc79519e0250979aa60
withkeygames <withkeygames@gmail.com>
2022-09-24 20:57:11 +0300

    Solved

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,12 +1,25 @@
 package com.efimchick.ifmo.streams.countwords;
 
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Words {
 
-    public String countWords(List<String> lines) {
+    static final int MIN_LENGTH = 4;
+    static final int MIN_FREQUENCY = 10;
 
-        return null;
+    public String countWords(List<String> lines) {
+        String string = lines.stream().flatMap(line -> Arrays.stream(line.split("\\P{javaLetter}+")))
+                .map(String::toLowerCase).filter(a -> a.length() >= MIN_LENGTH).collect(Collectors.groupingBy(a -> a, Collectors.counting()))
+                .entrySet().stream().filter(collectList -> collectList.getValue() >= MIN_FREQUENCY).sorted(Map.Entry.comparingByKey())
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue())).map(a -> String.format("%s - %d\n", a.getKey(), a.getValue())).collect(Collectors.joining());
+
+        StringBuilder stringBuilder = new StringBuilder(string);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        return stringBuilder.toString();
+
     }
 }
